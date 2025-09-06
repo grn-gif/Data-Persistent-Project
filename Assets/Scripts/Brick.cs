3cedf83209b620be2b8fbd25e22ac9d8e23afe90
grn-gif <impetusquantum@gmail.com>
2025-09-06 17:23:42 +0100

    inital summit
    v3.1

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,11 +1,19 @@
+using System.Drawing.Imaging;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Brick : MonoBehaviour//Our Game Script
 {
     public UnityEvent<int> onDestroyed;
-    
+
     public int PointValue;
+    public static class Palette
+    {
+        public static readonly Color32 MidnightBlue = new Color32(27, 38, 59, 255);   // #1B263B
+        public static readonly Color32 TealGreen = new Color32(42, 157, 143, 255); // #2A9D8F
+        public static readonly Color32 SunsetOrange = new Color32(231, 111, 81, 255); // #E76F51
+        public static readonly Color32 SandBeige = new Color32(244, 162, 97, 255); // #F4A261
+    }
 
     void Start()
     {
@@ -15,16 +23,16 @@ public class Brick : MonoBehaviour//Our Game Script
         switch (PointValue)
         {
             case 1 :
-                block.SetColor("_BaseColor", Color.green);
+                block.SetColor("_BaseColor", Palette.MidnightBlue);
                 break;
             case 2:
-                block.SetColor("_BaseColor", Color.yellow);
+                block.SetColor("_BaseColor", Palette.TealGreen);
                 break;
             case 5:
-                block.SetColor("_BaseColor", Color.blue);
+                block.SetColor("_BaseColor", Palette.SunsetOrange);
                 break;
             default:
-                block.SetColor("_BaseColor", Color.red);
+                block.SetColor("_BaseColor", Palette.SandBeige);
                 break;
         }
         renderer.SetPropertyBlock(block);
